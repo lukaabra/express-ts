06b42c60afe16c4db4aa00bf26862e40c225d956
abra <lukaabramovic2@gmail.com>
2023-02-07 22:33:49 +0100

    feat: adds user cretion on registration

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,6 +1,7 @@
 import { Prisma } from '@prisma/client';
 import { Router, Request, Response } from 'express';
 
+import prisma from '../../prisma';
 import registerValidator from './validators/register';
 import validate from '../../middleware/validation';
 
@@ -12,9 +13,17 @@ apiRouter.post(
   '/register',
   registerValidator,
   validate,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const extendedReq = req as ExtendedRequest;
     const data = extendedReq.extractBody<Prisma.UserCreateInput>(req);
+
+    try {
+      await prisma.user.create({ data });
+    } catch (error) {
+      // TODO: Handle taken email error
+      console.error(error);
+      return res.status(500).send('Error creating user');
+    }
 
     return res.status(201).send('User created');
   }
